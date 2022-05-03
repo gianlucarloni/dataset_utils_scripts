d765c3f6a686d5e60a9853aa88a249370ec59d03
andreaberti11235 <andrea.berti11235@icloud.com>
2022-05-03 15:26:23 +0200

    minor fixes

--- ag_prepareForEarlyStopping_loss.py
+++ ag_prepareForEarlyStopping_loss.py
@@ -9,8 +9,8 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-valid_txt_path = '/media/si-lab/63bc1baf-d08c-4db5-b271-e462f3f4444d/a_e_g/ppnet_originale_originale/ProtoPNet/saved_models_baseline/resnet18/CBIS_baseline_massBenignMalignant_resnet18_mar_12_apr_2022_08:13:12_binaryCrossEntr/val_metrics.txt'
-# valid_txt_path ='/media/si-lab/63bc1baf-d08c-4db5-b271-e462f3f4444d/a_e_g/ppnet_originale_originale/ProtoPNet/saved_models_baseline/resnet18/CBIS_baseline_massBenignMalignant_resnet18_Tue_12_Apr_2022_09:45:31_binaryCrossEntr/val_metrics.txt'
+# valid_txt_path = '/media/si-lab/63bc1baf-d08c-4db5-b271-e462f3f4444d/a_e_g/ppnet_cluster/saved_models_baseline/resnet18_finetuning_last_3_layers_224_imgsize/CBIS_resnet18_finetuning_last_3_layers_224_imgsize_Wed_27_Apr_2022_13:25:32_binaryCrossEntr/val_metrics.txt'
+valid_txt_path ='/media/si-lab/63bc1baf-d08c-4db5-b271-e462f3f4444d/a_e_g/ppnet_originale_originale/ProtoPNet/saved_models_baseline/resnet18/CBIS_baseline_massBenignMalignant_resnet18_Tue_12_Apr_2022_09:45:31_binaryCrossEntr/val_metrics.txt'
 # valid_txt_path = '/media/si-lab/63bc1baf-d08c-4db5-b271-e462f3f4444d/a_e_g/ppnet_originale_originale/ProtoPNet/saved_models_baseline/resnet34/CBIS_baseline_massBenignMalignant_resnet34_sab_09_apr_2022_21:37:22_binaryCrossEntr/val_metrics.txt'
 
 df = pd.read_csv(valid_txt_path)
@@ -26,7 +26,7 @@ deriv = deriv[:-1]
 # deriv_o25 = [(e[2],(e[0]-e[1])/25) for idx,e in enumerate(zip(loss,loss_2,epochs)) if idx%25==0]
 # deriv_o25 = deriv_o25[:-1]
 
-window = 20
+window = 5 #20
 loss_npy = np.array(loss,dtype=float)
 windowed = [np.mean(loss_npy[i:i+window]) for i in range(0,len(loss),window)]
 windowed_epochs = range(window,len(loss)+window,window)
