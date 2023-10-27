3cbb60fe6ddb4e334ba4a149cb076dcc44557d72
Andrea Berti <andrea.berti11235@icloud.com>
2023-10-27 17:18:43 +0200

    defined out_path and out_path_label outside of the IF condition

--- create_yolo_mammo_dataset.py
+++ create_yolo_mammo_dataset.py
@@ -218,11 +218,10 @@ for ind, csv_path in enumerate([csv_mass_train_path, csv_mass_test_path]):
                         
                         # se è il primo record_per_patient creo il file della label e salvo
                         # l'immagine in png, altrimenti apro solo il file in append e aggiungo il nuovo bbox
+                        out_path = os.path.join(img_dir, out_img_name)
+                        out_path_label = os.path.join(label_dir, out_txt_label)
                         if num_record == 1:
                             # the structure will be dataset/train/images and dataset/train/labels (same thing for test)
-                            out_path = os.path.join(img_dir, out_img_name)
-                            out_path_label = os.path.join(label_dir, out_txt_label)
-
                             with open(out_path_label, "w") as label_file:
                                 label_file.write(f'0 {col_center} {row_center} {width} {height}')
 
